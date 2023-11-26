6ced1c50a276a79803399e909baba6a2de11d696
zvoleg <oleg.zaitsevv@gmail.com>
2023-11-26 12:34:51 +0300

    remove unnecessary comments

--- frame_reader.js
+++ frame_reader.js
@@ -6,7 +6,6 @@ var socket = new WebSocket("ws://localhost:3000/nes");
 socket.binaryType = "arraybuffer";
 
 socket.onmessage = function(event) {
-    // var bytes = Uint8Array.from(atob(event.data), c => c.charCodeAt(0));
     var imgData = new Uint8ClampedArray(event.data);
     var image = ctx.createImageData(256,244);
     for (var i = 0; i < image.data.length; i += 1) {
@@ -16,10 +15,8 @@ socket.onmessage = function(event) {
     var resizeHeight = 488;
 
     Promise.all([
-        // Cut out two sprites from the sprite sheet
         createImageBitmap(image)
       ]).then((img) => {
-        // Draw each sprite onto the canvas
         ctx.drawImage(img[0], 0, 0);
       });
 }
