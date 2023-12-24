bfac24ad66567794a9ab90a1f4acc4848e01189b
zvoleg <oleg.zaitsevv@gmail.com>
2023-12-24 20:06:48 +0300

    gui page table

--- frame_reader.js
+++ frame_reader.js
@@ -10,7 +10,7 @@ nameTableSocket.binaryType = "arraybuffer";
 setupSocketAndCanvas(frameSocket, "frame", 256, 240, 3)
 setupSocketAndCanvas(palletteSocket, "pallette", 9, 5, 20)
 setupSocketAndCanvas(patternSocket, "pattern", 256, 128, 2)
-setupSocketAndCanvas(nameTableSocket, "nameTable", 512, 480, 1)
+setupSocketAndCanvas(nameTableSocket, "nameTable", 512, 512, 1)
 
 function setupSocketAndCanvas(socket, canvasName, width, height, scale) {
   const canvas = document.getElementById(canvasName);
@@ -18,22 +18,19 @@ function setupSocketAndCanvas(socket, canvasName, width, height, scale) {
   canvas.style.height = (height * scale) + "px";
   canvas.width = width;
   canvas.height = height;
-  canvas.style.imageRendering = "pixelated"
-  const ctx = canvas.getContext("2d");
+  canvas.style.imageRendering = "pixelated";
+  const ctx = canvas.getContext("2d", { willReadFrequently: true });
   ctx.imageSmoothingEnabled = false;
-
+  
   socket.onopen = () => {
     socket.send(canvasName);
   }
-
+  
   socket.onmessage = (event) => {
-      var imgData = new Uint8ClampedArray(event.data);
-      var image = ctx.getImageData(0, 0, width, height);
-      for (var i = 0; i < image.data.length; i += 1) {
-          image.data[i] = imgData[i];
-      }
-
-      ctx.putImageData(image, 0, 0);
+    let imgData = new Uint8ClampedArray(event.data);
+    let img = ctx.getImageData(0, 0, width, height);
+    img.data.set(imgData);
+    ctx.putImageData(img, 0, 0);
   }
 
   socket.onerror = (err) => {
